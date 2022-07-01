1d11aae87220ae4181ed6b242cc1e985ee1ee872
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2022-07-01 12:10:55 -0400

    Avanco2

--- Source/AlterarDadosProduto.c
+++ Source/AlterarDadosProduto.c
@@ -6,52 +6,8 @@
 #include "Registros.h"
 
 void alterarDadosProduto(char arquivoProdutos[]) {
-    int opcao;
-    bool valido;
 
-    imprimirEstoqueProdutos(arquivoProdutos);
 
-    printf("\nDigite o código do produto que deseja alterar:");
-    scanf(" %d", &opcao);
-    opcao--;
-    limpaTela();
 
-    if (opcao >= 0 && opcao < codigo) {
-        tProduto *novo = malloc(sizeof(tProduto));
 
-        novo->codigo = opcao + 1;
-        valido = false;
-        while (!valido) {
-            printf("Setor: ");
-            scanf(" %[^\n]s", novo->setor);
-            if (strcmp(novo->setor, "Higiene e limpeza") == 0 ||
-                strcmp(novo->setor, "Bebidas") == 0 ||
-                strcmp(novo->setor, "Frios") == 0 || strcmp(novo->setor, "Padaria") == 0 ||
-                strcmp(novo->setor, "Açougue") == 0) {
-                valido = true;
-            } else {
-                printf("\nSetor inválido, digite um setor válido\n\n");
-            }
-        }
-        printf("Nome: ");
-        scanf(" %[^\n]s", novo->nome);
-        printf("Preço: ");
-        scanf(" %lf", &novo->preco);
-        printf("Data de validade (dd/mm/aaaa): ");
-        scanf(" %d/%d/%d", &novo->validade.dia, &novo->validade.mes,
-              &novo->validade.ano);
-        valido = false;
-        while (!valido) {
-            printf("Quantidade em estoque: ");
-            scanf(" %d", &novo->estoque);
-            if (novo->estoque >= 0) {
-                valido = true;
-            } else {
-                printf("\nQuantidade inválida, digite uma quantidade válida\n\n");
-            }
-        }
-        produto[opcao] = novo;
-        limpaTela();
-    } else
-        printf("Código inválido\n\n");
 }
--- Source/Teste.c
+++ Source/Teste.c
@@ -1,51 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
+typedef struct {
     char nome[50];
     int dia, mes, ano;
-}Contato;
+} Contato;
 
-void salvarB(char arq[], Contato **c, int quant){
+void salvarB(char arq[], Contato **c, int quant) {
     FILE *file = fopen(arq, "wb");
     int i;
 
-    if(file){
-        for(i = 0; i < quant; i++)
+    if (file) {
+        for (i = 0; i < quant; i++)
             fwrite(c[i], sizeof(Contato), 1, file);
         fclose(file);
-    }
-    else
+    } else
         printf("\nErro ao abrir arquivo!\n");
 }
 
-int lerB(char arq[], Contato **c){
+int lerB(char arq[], Contato **c) {
     int quant = 0;
     Contato *novo = malloc(sizeof(Contato));
     FILE *file = fopen(arq, "rb");
 
-    if(file){
-        while(fread(novo, sizeof(Contato), 1, file)){
+    if (file) {
+        while (fread(novo, sizeof(Contato), 1, file)) {
             c[quant] = novo;
             quant++;
             novo = malloc(sizeof(Contato));
         }
         fclose(file);
-    }
-    else
+    } else
         printf("\nErro ao abrir arquivo!\n");
     return quant;
 }
 
-void alterarB(char arq[]){
+void alterarB(char arq[]) {
     FILE *file = fopen(arq, "rb+");
     Contato c;
     int id, i = 1;
 
-    if(file){
+    if (file) {
         printf("\tLista de contatos:\n");
         printf("\t----------------------------\n");
-        while(fread(&c, sizeof(Contato), 1, file)){
+        while (fread(&c, sizeof(Contato), 1, file)) {
             printf("\t%d = %2d/%2d/%4d\t%s\n", i, c.dia, c.mes, c.ano, c.nome);
             i++;
         }
@@ -56,31 +54,30 @@ void alterarB(char arq[]){
         getchar();
         id--;
 
-        if(id >= 0 && id < i - 1){
+        if (id >= 0 && id < i - 1) {
             printf("\tDigite nome e data de nascimento dd mm aaaa: ");
             scanf("%49[^\n]%d%d%d", c.nome, &c.dia, &c.mes, &c.ano);
             fseek(file, id * sizeof(Contato), SEEK_SET);
             fwrite(&c, sizeof(Contato), 1, file);
         }
         fclose(file);
-    }
-    else
+    } else
         printf("\nErro ao abrir arquivo!\n");
 }
 
-int main(){
+int main() {
 
     Contato *agenda[50];
     char arquivo2[] = {"agenda.dat"};
     int opcao, quant = 0;
 
-    do{
+    do {
         printf("\n\t0 - Sair\n\t1 - Cadastrar\n\t2 - Alterar\n\t3 - Imprimir\n");
         printf("\t4 - Salvar\n\t5 - Ler arquivo\n\t6 - SalvarB\n\t7 - LerB\n\t8 - AltererB\n");
         scanf("%d", &opcao);
         getchar();
 
-        switch(opcao){
+        switch (opcao) {
             case 6:
                 salvarB(arquivo2, agenda, quant);
                 break;
@@ -91,10 +88,10 @@ int main(){
                 alterarB(arquivo2);
                 break;
             default:
-                if(opcao != 0)
+                if (opcao != 0)
                     printf("\n\tOpcao invalida!!!\n");
         }
-    }while(opcao != 0);
+    } while (opcao != 0);
 
     return 0;
 }
