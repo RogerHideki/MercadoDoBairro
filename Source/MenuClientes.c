80af8898f110c0ce14ae13e47b225587628f3be7
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2022-07-03 16:51:37 -0400

    FaltaVendas

--- Source/MenuClientes.c
+++ Source/MenuClientes.c
@@ -14,10 +14,9 @@ void menuClientes() {
     do {
         //Imprime o menu de clientes
         printf("1 - Cadastrar um novo cliente\n");
-        printf("2 - Atualizar pontuação de um cliente\n");
-        printf("3 - Atualizar dados de um cliente\n");
-        printf("4 - Mostrar a quantidade de clientes com idade entre 18 e 25 anos\n");
-        printf("5 - Listar clientes com mais de 1000 pontos\n");
+        printf("2 - Atualizar dados de um cliente\n");
+        printf("3 - Mostrar a quantidade de clientes com idade entre 18 e 25 anos\n");
+        printf("4 - Listar clientes com mais de 1000 pontos\n");
         printf("9 - Voltar\n");
 
         //Pede uma opção, anota a opção escolhida e depois limpa a tela
@@ -31,15 +30,12 @@ void menuClientes() {
                 codigo += cadastrarCliente(codigo);
                 break;
             case 2:
-
-                break;
-            case 3:
                 alterarDadosCliente();
                 break;
-            case 4:
+            case 3:
                 quantidadeClientesIdadeEntre18E25();
                 break;
-            case 5:
+            case 4:
                 clientesMais1000Pontos();
                 break;
             default:
--- Source/QuantidadeClientesIdadeEntre18E25.c
+++ Source/QuantidadeClientesIdadeEntre18E25.c
@@ -2,20 +2,19 @@
 #include "Funcoes.h"
 #include "Registros.h"
 
-void clientesMais1000Pontos() {
+void quantidadeClientesIdadeEntre18E25() {
     tCliente cliente;
-    int i;
+    int i = 0;
     FILE *fClientes;
 
     fClientes = fopen("../Arquivos/Cliente.dat", "rb");
 
     if (fClientes) {
         while (fread(&cliente, sizeof(tCliente), 1, fClientes)) {
-            if (cliente.idade >= 18 && cliente.idade <= 25) {
-                printf("%d", cliente.pontos);
-            }
+            if (cliente.idade >= 18 && cliente.idade <= 25)
+                i++;
         }
-        printf("Quantidade de clientes com idade entre 18 e 25 anos:\n");
+        printf("Quantidade de clientes com idade entre 18 e 25 anos: %d clientes\n\n", i);
         fclose(fClientes);
     } else {
         limpaTela();
