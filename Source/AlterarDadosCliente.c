e2c0f1f96ed475eb609a2092fb10d0c4d099799b
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2022-07-02 23:17:44 -0400

    GrandeAtualizacao

--- Source/AlterarDadosCliente.c
+++ Source/AlterarDadosCliente.c
@@ -11,9 +11,9 @@ void alterarDadosCliente() {
         printf("Lista de Clientes:\n");
         printf("------------------------------------------------------------------------------------------------------------------------------------------\n");
         while (fread(&cliente, sizeof(tCliente), 1, fClientes)) {
-            printf("%d - %13s\t%50s\t%2d/%2d/%4d\t%d anos\t%50s\t%50s\t%2s\t%d\n", cliente.codigo, cliente.cpf, cliente.nome,
-                   cliente.dataNascimento.dia, cliente.dataNascimento.mes, cliente.dataNascimento.ano, cliente.idade,
-                   cliente.endereco, cliente.cidade, cliente.estado, cliente.pontos);
+            printf("%d - %13s\t%50s\t%2d/%2d/%4d\t%d anos\t%50s\t%50s\t%2s\t%d pontos\n", cliente.codigo, cliente.cpf,
+                   cliente.nome, cliente.dataNascimento.dia, cliente.dataNascimento.mes, cliente.dataNascimento.ano,
+                   cliente.idade, cliente.endereco, cliente.cidade, cliente.estado, cliente.pontos);
         }
         printf("------------------------------------------------------------------------------------------------------------------------------------------\n");
 
@@ -24,36 +24,7 @@ void alterarDadosCliente() {
 
         if (opcao >= 0 && opcao < cliente.codigo) {
             cliente.codigo = opcao + 1;
-            valido = false;
-            while (!valido) {
-                printf("Setor: ");
-                scanf(" %[^\n]s", cliente.setor);
-                if (strcmp(cliente.setor, "Higiene e limpeza") == 0 ||
-                    strcmp(cliente.setor, "Bebidas") == 0 ||
-                    strcmp(cliente.setor, "Frios") == 0 || strcmp(cliente.setor, "Padaria") == 0 ||
-                    strcmp(cliente.setor, "Açougue") == 0) {
-                    valido = true;
-                } else {
-                    printf("\nSetor inválido, digite um setor válido\n\n");
-                }
-            }
-            printf("Nome: ");
-            scanf(" %[^\n]s", cliente.nome);
-            printf("Preço: ");
-            scanf(" %lf", &cliente.preco);
-            printf("Data de validade (dd/mm/aaaa): ");
-            scanf(" %d/%d/%d", &cliente.validade.dia, &cliente.validade.mes,
-                  &cliente.validade.ano);
-            valido = false;
-            while (!valido) {
-                printf("Quantidade em estoque: ");
-                scanf(" %d", &cliente.estoque);
-                if (cliente.estoque >= 0) {
-                    valido = true;
-                } else {
-                    printf("\nQuantidade inválida, digite uma quantidade válida\n\n");
-                }
-            }
+
             fseek(fClientes, opcao * sizeof(tCliente), SEEK_SET);
             fwrite(&produto, sizeof(tCliente), 1, fClientes);
             limpaTela();
