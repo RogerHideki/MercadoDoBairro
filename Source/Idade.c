c1e9e47fc365d6c7b70b19917114a58247dfee03
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2022-07-02 22:39:15 -0400

    Update Idade.c

--- Source/Idade.c
+++ Source/Idade.c
@@ -11,13 +11,16 @@ int idade(tCliente cliente) {
 
     if ((DataAtual->tm_year + 1900) - cliente.dataNascimento.ano > 0) {
         if ((DataAtual->tm_mon + 1) - cliente.dataNascimento.mes < 0)
-            return ((DataAtual->tm_year + 1900) - cliente.dataNascimento.ano);
-        else if ((DataAtual->tm_mon + 1) - cliente.dataNascimento.mes == 0)
-
+            return ((DataAtual->tm_year + 1900) - cliente.dataNascimento.ano - 1);
+        else if ((DataAtual->tm_mon + 1) - cliente.dataNascimento.mes == 0) {
+            if ((DataAtual->tm_mday) - cliente.dataNascimento.dia);
+                return ();
+            else
+                return ();
+        }
         else if ((DataAtual->tm_mon + 1) - cliente.dataNascimento.mes > 0)
+            return ((DataAtual->tm_year + 1900) - cliente.dataNascimento.ano);
     }
-
-    DataAtual->tm_mday;
-
-    return 0;
+    else
+        return 0;
 }
