47cc5b92aec81bbe8e08d3dd9bcbb75b1af0d547
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2022-07-03 22:43:23 -0400

    ComecoVenda2

--- Source/CadastrarVenda.c
+++ Source/CadastrarVenda.c
@@ -1,56 +1,18 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
 #include "Funcoes.h"
 #include "Registros.h"
 
-int cadastrarProduto(int codigo) {
-    tProduto produto;
-    bool valido;
-    FILE *fProdutos = fopen("../Arquivos/Produtos.dat", "ab");
+int cadastrarVenda(int codigo) {
+    tVenda venda;
+    FILE *fClientes = fopen("../Arquivos/Cliente.dat", "rb+");
+    FILE *fVendas = fopen("../Arquivos/Vendas.dat", "ab");
 
-    if (fProdutos) {
-        produto.codigo = codigo;
-        valido = false;
-        while (!valido) {
-            printf("Setor: ");
-            scanf(" %[^\n]s", produto.setor);
-            if (strcmp(produto.setor, "Higiene e limpeza") == 0 ||
-                strcmp(produto.setor, "Bebidas") == 0 ||
-                strcmp(produto.setor, "Frios") == 0 || strcmp(produto.setor, "Padaria") == 0 ||
-                strcmp(produto.setor, "Açougue") == 0) {
-                valido = true;
-            } else {
-                printf("\nSetor inválido, digite um setor válido\n\n");
-            }
-        }
-        printf("Nome: ");
-        scanf(" %[^\n]s", produto.nome);
-        valido = false;
-        while (!valido) {
-            printf("Preço: ");
-            scanf(" %lf", &produto.preco);
-            if (produto.preco >= 0) {
-                valido = true;
-            } else {
-                printf("\nPreço inválido, digite um preço válido\n\n");
-            }
-        }
-        printf("Data de validade (dd/mm/aaaa): ");
-        scanf(" %d/%d/%d", &produto.validade.dia, &produto.validade.mes,
-              &produto.validade.ano);
-        valido = false;
-        while (!valido) {
-            printf("Quantidade em estoque: ");
-            scanf(" %d", &produto.estoque);
-            if (produto.estoque >= 0) {
-                valido = true;
-            } else {
-                printf("\nQuantidade inválida, digite uma quantidade válida\n\n");
-            }
-        }
-        fwrite(&produto, sizeof(tProduto), 1, fProdutos);
-        fclose(fProdutos);
+    if (fVendas) {
+        venda.codigo = codigo;
+
+        fwrite(&venda, sizeof(tVenda), 1, fVendas);
+        fclose(fVendas);
         limpaTela();
     } else {
         limpaTela();
